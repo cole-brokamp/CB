b170a61be99819b0dd8358bf41d2777976ddac57
Cole Brokamp <cole.brokamp@gmail.com>
2015-08-26 17:14:31 -0400

    added save_pdf function

---
+++ R/save_pdf.R
@@ -0,0 +1,9 @@
+#' save_pdf
+#'
+#' Copies the graphics contents of current device to PDF (a wrapper for \code{dev2.pdf}). Default size is 8.5 x 11 in landscape mode.
+#' @param file filename to save the image
+#' @param width width of pdf image
+#' @param height height of pdf image
+#' @export
+
+save_pdf <- function(file,width=11,height=8.5) invisible(dev.copy2pdf(file=file,width=width,height=height))
