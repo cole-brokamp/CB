ec515e12d0d084c88652c49884c77ecc7940bad1
Cole Brokamp <cole.brokamp@gmail.com>
2016-04-21 17:58:35 -0400

    more notes...

--- CBapply_new.R
+++ CBapply_new.R
@@ -17,10 +17,16 @@ names(X) <- LETTERS[1:10]
 
 CBapply(X,function(x) data.frame('mean'=mean(x)),
         output='data.frame',fill=F,parallel=F,names='row.names',pb=F)
+
+
 CBapply(X,mean,
         output='data.frame',fill=F,parallel=T,names='row.names',pb=T,na.rm=T)
 
-
+# example toy function
+FUN. <- function(x) {
+  Sys.sleep(1)
+  mean(x)
+}
 
 CBapply <- function(X,FUN.,output='data.frame',fill=FALSE,parallel=FALSE,
                     num.cores=NULL,...,names='row.names',pb=TRUE) {
@@ -29,6 +35,7 @@ CBapply <- function(X,FUN.,output='data.frame',fill=FALSE,parallel=FALSE,
             num.cores > 0 | is.null(num.cores),
             names %in% c('row.names','names'))
 
+  # figure out way to name output column in data.frame if already doesn't return a data.frame
   FUN <- function(x,...) as.data.frame(FUN.(x,...))
 
   n <- length(X)
@@ -42,51 +49,50 @@ CBapply <- function(X,FUN.,output='data.frame',fill=FALSE,parallel=FALSE,
     if (is.na(n.cores)) n.cores <- 1
   }
 
-    if (pb & (!parallel)) {
-      tmp <- vector('list', n)
-      pb <- progress::progress_bar$new(total=100,
-                                       format='   ...  :what (:percent)   [ ETA: :eta | Elapsed: :elapsed ]',
-                                       clear=FALSE,force=FALSE)
-      for (i in 1:n) {
-        pb$tick(len=100/n,tokens = list(what = paste0('processing ',i,' of ',n)))
-        tmp[[i]] <- FUN(X[[i]],...)
-      }
+  if (pb & (!parallel)) {
+    tmp <- vector('list', n)
+    pb <- progress::progress_bar$new(total=100,
+                                     format='...  :what (:percent)   [ ETA: :eta | Elapsed: :elapsed ]',
+                                     clear=FALSE,force=FALSE,show_after=0)
+    for (i in 1:n) {
+      pb$tick(len=100/n,tokens = list(what = paste0('processing ',i,' of ',n)))
+      tmp[[i]] <- FUN(X[[i]],...)
     }
+  }
 
-    if (pb & parallel) {
-      wrapFUN <- function(i) {
-        message(paste0('   ... processing ',i,' of ',n))
-        out <- FUN(X[[i]],...)
-        return(out)
-      }
-      wrapFUN(1)
-      tmp <- parallel::mclapply(1:n,wrapFUN,mc.cores=n.cores,mc.silent=F,mc.cleanup=T)
+  if (pb & parallel) {
+    wrapFUN <- function(i,...) {
+      message(paste0('   ... processing ',i,' of ',n))
+      out <- FUN(X[[i]],...)
+      return(out)
     }
+    tmp <- parallel::mclapply(1:n,wrapFUN,mc.cores=n.cores,mc.silent=F,mc.cleanup=T)
+  }
 
 
-    if ((!pb) & parallel) tmp <- parallel::mclapply(X,FUN,mc.cores=num.cores,...)
-    if ((!pb) & (!parallel)) tmp <- lapply(X,FUN,...)
+  if ((!pb) & parallel) tmp <- parallel::mclapply(X,FUN,mc.cores=num.cores,...)
+  if ((!pb) & (!parallel)) tmp <- lapply(X,FUN,...)
 
-    if (output=='data.frame') {
-      fillFUN <- ifelse(fill,plyr::rbind.fill,rbind)
-      tmp <- do.call(fillFUN,tmp)
-    }
+  if (output=='data.frame') {
+    fillFUN <- ifelse(fill,plyr::rbind.fill,rbind)
+    tmp <- do.call(fillFUN,tmp)
+  }
 
-    if (is.null(names(X))) {
-      apply_names <- as.character(X)
+  if (is.null(names(X))) {
+    apply_names <- as.character(X)
+  } else {
+    apply_names <- names(X)
+  }
+
+  if ((names == 'row.names') & (output == 'data.frame')) {
+    if( ! all(sapply(tmp,nrow) ==  1)) {
+      warning('output from function is not data.frame with 1 row\n ... putting names in "names" column')
+      tmp$names <- rep(apply_names,each=sapply(tmp,nrow))
     } else {
-      apply_names <- names(X)
-    }
-
-    if ((names == 'row.names') & (output == 'data.frame')) {
-      if( ! all(sapply(tmp,nrow) ==  1)) {
-        warning('output from function is not data.frame with 1 row\n ... putting names in "names" column')
-        tmp$names <- rep(apply_names,each=sapply(tmp,nrow))
-      } else {
-        row.names(tmp) <- apply_names
-      }
       row.names(tmp) <- apply_names
     }
+    row.names(tmp) <- apply_names
+  }
   if (names == 'names' & (output == 'data.frame')) tmp$names <- rep(apply_names,each=sapply(tmp,nrow))
   if (output == 'list') names(tmp) <- apply_names
 
