ce3aeb062250fd85112420c14dae7d65dd741ae0
Cole Brokamp <cole.brokamp@gmail.com>
2017-02-19 21:29:29 -0500

    round_df default to 2 digits

--- R/round_df.R
+++ R/round_df.R
@@ -5,7 +5,7 @@
 #'
 #' @return rounded data.frame
 #' @export
-round_df <- function(x, digits) {
+round_df <- function(x, digits=2) {
   numeric_columns <- sapply(x, class) == 'numeric'
   x[numeric_columns] <-  round(x[numeric_columns], digits)
   x
