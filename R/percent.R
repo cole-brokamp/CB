3dd42944f6d8369d331d315142b61599f4ae3d72
Cole Brokamp <cole.brokamp@gmail.com>
2017-04-08 15:57:05 -0400

    add paren option for printing percents

--- R/percent.R
+++ R/percent.R
@@ -2,14 +2,18 @@
 #'
 #' @param x decimal
 #' @param digits number of digits to show after the decimal place
+#' @param paren logical; print with surrounding parentheses?
 #'
 #' @export
 #'
 #' @examples
 #' percent(0.6770471)
+#' percent(0.6770471, digits=0, paren=TRUE)
 
-percent <- function(x,digits=1) {
-  paste0(formatC(100 * x,format='f',digits=digits),"%")
+percent <- function(x, digits=1, paren=FALSE) {
+  out <- paste0(formatC(100 * x,format='f',digits=digits),"%")
+  if (paren) out <- paste0('(', out, ')')
+  return(out)
 }
 
 
