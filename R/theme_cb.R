2df81530ee36f0bb26def19af8acb4366d3d60fe
Cole Brokamp <cole.brokamp@gmail.com>
2017-05-24 16:46:54 -0400

    more updates to ggplot theme

--- R/theme_cb.R
+++ R/theme_cb.R
@@ -8,18 +8,39 @@
 #' @examples
 #' library(ggplot2)
 #' ggplot(mtcars, aes(wt, mpg)) +
-#' geom_point() +
-#'  labs(title = "A Plot",
-#'       subtitle = "Look, a subtitle!") +
-#'  theme_cb()
-#'
-theme_cb <- function(base_size = 11, ...) {
-  out <- ggplot2::theme_minimal(base_family = "RobotoCondensed-Regular",
-                                base_size = base_size, ...)
-  out$plot.title <- ggplot2::element_text(family="Roboto-Bold",
-                                          size = ggplot2::rel(1.2),
-                                          hjust = 0, vjust = 1,
-                                          margin = ggplot2::margin(b = base_size / 2 * 1.2)
-  )
+#` geom_point() +
+#`   labs(title = "A Plot",
+#`        subtitle = "Look, a subtitle!") +
+#`   theme_cb()
+#`
+#` ggplot(mtcars, aes(wt, mpg)) +
+#`   geom_point() +
+#`   labs(title = "A Plot",
+#`        subtitle = "Look, a subtitle!") +
+#`   facet_grid(~vs) +
+#`   theme_cb()
+#`
+#` ggplot(mtcars, aes(wt, mpg, color=factor(vs))) +
+#`   geom_point() +
+#`   labs(title = "A Plot",
+#`        subtitle = "Look, a subtitle!") +
+#`   theme_cb()
+theme_cb <- function (base_size = 11, ...){
+  out <- ggplot2::theme_light(base_family = "Arial", base_size = base_size)
+  out <- out + theme(panel.background = element_rect(fill = "white", colour = NA),
+                     panel.border = element_rect(fill = NA, colour = "grey20"),
+                     panel.grid.major = element_line(colour = "grey92"),
+                     panel.grid.minor = element_blank(),
+                     strip.background = element_rect(fill = "grey92", colour = "grey20"),
+                     strip.text = element_text(color='grey20'),
+                     legend.key = element_rect(fill = "white", colour = NA),
+                     complete = TRUE, ...)
+  out$plot.title <- ggplot2::element_text(family = "Arial",
+                                          face='bold',
+                                          size = ggplot2::rel(1.2), hjust = 0, vjust = 1,
+                                          margin = ggplot2::margin(b = base_size/2 * 1.2))
   out
 }
+
+
+
