cd3548be9f31330172aa7af79ad803e2cf4652c0
Cole Brokamp <cole.brokamp@gmail.com>
2017-05-24 12:30:20 -0400

    add custom cb ggplot theme

---
+++ R/theme_cb.R
@@ -0,0 +1,24 @@
+#' Custom CB ggplot2 theme
+#'
+#' @param base_size base font size
+#' @param ... other arguments passed to \code{ggplot2::theme_*()}
+#'
+#' @export
+#'
+#' @examples
+#' ggplot(mtcars, aes(wt, mpg)) +
+#' geom_point() +
+#'  labs(title = "A Plot",
+#'       subtitle = "Look, a subtitle!") +
+#'  theme_cb()
+#'
+theme_cb <- function(base_size = 11, ...) {
+  out <- ggplot2::theme_minimal(base_family = "RobotoCondensed-Regular",
+                                base_size = base_size, ...)
+  out$plot.title <- ggplot2::element_text(family="Roboto-Bold",
+                                          size = rel(1.2),
+                                          hjust = 0, vjust = 1,
+                                          margin = ggplot2::margin(b = base_size / 2 * 1.2)
+  )
+  out
+}
