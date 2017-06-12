e7144b0804ca44f15e7387a0a1b95f2d3d9ecb02
Cole Brokamp <cole.brokamp@gmail.com>
2017-06-12 17:44:48 -0400

    theme_set namespace issue resolved

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .onAttach <- function(...) {
   if (!interactive()) return()
   packageStartupMessage('setting CB::theme_bw() to ggplot default via ggplot2::set_theme()')
-  theme_set(CB::theme_cb())
+  ggplot2::theme_set(CB::theme_cb())
 }
 
