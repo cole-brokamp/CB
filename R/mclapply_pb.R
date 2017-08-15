6fd87648e100824389b1c43feb65e3af1b3f8ea5
Cole Brokamp <cole.brokamp@gmail.com>
2017-08-15 15:56:32 -0400

    add ETA to parallel progress bar

--- R/mclapply_pb.R
+++ R/mclapply_pb.R
@@ -8,57 +8,36 @@
 ##'                  objects (including classed objects) will be coerced by
 ##'                  ‘as.list’
 ##' @param FUN       the function to be applied to
-##' @param ...       optional arguments to ‘FUN’
-##' @param mc.preschedule see mclapply
-##' @param mc.set.seed see mclapply
-##' @param mc.silent see mclapply
-##' @param mc.cores see mclapply
-##' @param mc.cleanup see mclapply
-##' @param mc.allow.recursive see mclapply
-##' @param mc.progress track progress?
+##' @param mc.cores number of cores to use
 ##'
-##------------------------------------------------------------------------------
-mclapply_pb <- function(X, FUN, ...,
-                        mc.preschedule = TRUE, mc.set.seed = TRUE,
-                        mc.silent = FALSE, mc.cores = getOption("mc.cores", 2L),
-                        mc.cleanup = TRUE, mc.allow.recursive = TRUE,
-                        mc.progress=TRUE)
-{
-  if (Sys.getenv("RSTUDIO") == "1") message("progress bar doesn't work in RStudio!")
-  library(parallel)
-  if (!is.vector(X) || is.object(X)) X <- as.list(X)
-
-  if (mc.progress) {
+mclapply_pb <- function(X, FUN, mc.cores = getOption("mc.cores", 2L)){
+    if (Sys.getenv("RSTUDIO") == "1") message("progress bar doesn't work in RStudio!")
+    library(parallel)
+    if (!is.vector(X) || is.object(X)) X <- as.list(X)
+    n <- length(X)
     f <- fifo(tempfile(), open="w+b", blocking=T)
+    on.exit(close(f))
     p <- parallel:::mcfork()
-    pb <- utils::txtProgressBar(0, length(X), style=3)
-    utils::setTxtProgressBar(pb, 0)
+    pbb <- pbmcapply::progressBar(0, n, style='ETA', width=60)
+    utils::setTxtProgressBar(pbb, 0)
     progress <- 0
     if (inherits(p, "masterProcess")) {
-      while (progress < length(X)) {
-        readBin(f, "double")
-        progress <- progress + 1
-        utils::setTxtProgressBar(pb, progress)
-      }
-      cat("\n")
-      parallel:::mcexit()
+        while (progress < n) {
+            readBin(f, "double")
+            progress <- progress + 1
+            utils::setTxtProgressBar(pbb, progress)
+        }
+        cat("\n")
+        parallel:::mcexit()
     }
-  }
-  tryCatch({
-    result <- parallel::mclapply(X, function(.x,...) {
-      res <- FUN(.x,...)
-      if (mc.progress) writeBin(1, f)
-      res
-    },...,
-    mc.preschedule = mc.preschedule, mc.set.seed = mc.set.seed,
-    mc.silent = mc.silent, mc.cores = mc.cores,
-    mc.cleanup = mc.cleanup, mc.allow.recursive = mc.allow.recursive
-    )
-
-  }, finally = {
-    if (mc.progress) close(f)
-  })
-  result
+    result <- parallel::mclapply(X, function(.x) {
+                                     res <- FUN(.x)
+                                     writeBin(1, f)
+                                     res
+},
+mc.cores = mc.cores)
+    result
 }
+
 
 
