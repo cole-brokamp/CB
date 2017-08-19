7cc6f8a2a98fcb8aeca868cdc7c81bcdd6defc40
Cole Brokamp <cole.brokamp@gmail.com>
2017-08-18 20:13:44 -0400

    set default ggplot theme even when not interactive

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,5 @@
 .onAttach <- function(...) {
-  if (!interactive()) return()
-  packageStartupMessage('setting CB::theme_bw() to ggplot default via ggplot2::set_theme()')
+  if (!interactive()) packageStartupMessage('setting CB::theme_bw() to ggplot default via ggplot2::set_theme()')
   ggplot2::theme_set(CB::theme_cb())
 }
 
